a63aece16d390726bcb76ea63bd02f87cc16f99f
Elliot Plant <elliotaplant@gmail.com>
2017-11-19 16:37:58 -0800

    More subtle styling

--- src/AppFooter/AppFooter.jsx
+++ src/AppFooter/AppFooter.jsx
@@ -4,8 +4,9 @@ import './AppFooter.css';
 
 export default function AppFooter(props) {
   return (<footer className="app-footer">
-    <FontAwesome className="donate-icon" name="heart" size="2x"></FontAwesome>
+    <FontAwesome className="donate-icon" name="list" size="2x"></FontAwesome>
+    <FontAwesome className="info-icon" name="map" size="2x"></FontAwesome>
     <FontAwesome className="donate-icon" name="question-circle" size="2x"></FontAwesome>
-    <FontAwesome className="info-icon" name="info-circle" size="2x"></FontAwesome>
+    <FontAwesome className="info-icon" name="heart" size="2x"></FontAwesome>
   </footer>)
 }
